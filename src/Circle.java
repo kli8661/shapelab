a75e561d5a71affd232c3e5438997f86a37d0424
BT_1N3_01 <sjiang9453@bths.edu>
2018-10-02 10:12:37 -0400

    Added Circle Done

--- src/Circle.java
+++ src/Circle.java
@@ -3,15 +3,15 @@ public class Circle extends Shape
     private int side;
     public Circle(int side)
     {
-        super("Circle");
-        this.side = side;
+        super("Circle");//Constructive
+        this.side = side;// setting perameter to Field
     }
     public double getArea()
     {
         return Math.PI*side*side;
-    }
+    }//getting area using PI r squared
     public double getPerimeter()
     {
-        return Math.PI*2*side;
+        return Math.PI*2*side;//getting perimeter by 2 times PI times Radius;
     }
 }
--- src/ETriangle.java
+++ src/ETriangle.java
@@ -4,15 +4,15 @@ public class ETriangle extends Shape
 
     public ETriangle(int side)
     {
-        super("ETriangle");
-        this.side = side;
+        super("ETriangle");//Constructive
+        this.side = side;//Field side
     }
     public double getArea()
     {
-        return 0.433*(side*side);
+        return 0.433*(side*side);// returning SquareRoot of 3 divided by 4  and times the sidelength squared
     }
     public double getPerimeter()
     {
-        return side*3;
+        return side*3;//ETriangle have 3 equal side length. side*3 returns perimeter;
     }
 }
